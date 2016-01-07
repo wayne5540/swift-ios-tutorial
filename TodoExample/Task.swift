15ebc86eb74981679082f76367886e0b257c3d46
wayne <wayne.5540@gmail.com>
2016-01-07 17:34:23 +0800

    Add Task.swift

---
+++ TodoExample/Task.swift
@@ -0,0 +1,16 @@
+//
+//  Task.swift
+//  TodoExample
+//
+//  Created by Wayne on 1/7/16.
+//  Copyright © 2016 Wayne. All rights reserved.
+//
+
+import RealmSwift
+
+class Task: Object {
+    
+    dynamic var name = ""
+    dynamic var isCompleted = false
+    
+}
