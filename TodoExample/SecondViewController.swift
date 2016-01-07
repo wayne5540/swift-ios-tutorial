3c7276d472917330d6b70869e3d5cb74c496e75b
wayne <wayne.5540@gmail.com>
2016-01-07 17:11:25 +0800

    Step1 - init, finish view

---
+++ TodoExample/SecondViewController.swift
@@ -0,0 +1,28 @@
+//
+//  SecondViewController.swift
+//  TodoExample
+//
+//  Created by Wayne on 1/7/16.
+//  Copyright © 2016 Wayne. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    @IBAction func addNewItem(sender: AnyObject) {
+    }
+}
+
